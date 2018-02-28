f81cb16278caaa7aad70d48e1425b729113b0c0a
arnav-aggarwal <arnav.aggarwal@thecontrolgroup.com>
2018-02-28 12:53:06 -0800

    Refactor index.js for readability

--- index.js
+++ index.js
@@ -2,13 +2,14 @@ const fs = require('fs');
 const papa = require('papaparse');
 const script = require('./script');
 
-fs.readdirSync('./state-crime-data').forEach(fileName => {
-	const filePath = './state-crime-data/' + fileName;
+const dataFolderPath = './state-crime-data/';
+const fileNames = fs.readdirSync(dataFolderPath);
+fileNames.forEach(fileName => {
+	const filePath = dataFolderPath + fileName;
 	const fileContents = fs.readFileSync(filePath, 'utf8');
 	const parsedContents = papa.parse(fileContents).data;
-	const data = script(parsedContents);
 
+	const data = script(parsedContents);
 	const year = fileName.slice(0, 4);
 	fs.writeFileSync(`./formatted-data/test-${year}.json`, JSON.stringify(data, null, 2));
 });
-
