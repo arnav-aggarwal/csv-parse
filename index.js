0b2edb9c7b2599624bc89cb44b1f701698530533
arnav-aggarwal <arnav.aggarwal@thecontrolgroup.com>
2018-02-28 09:42:26 -0800

    Install papaparse

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 const fs = require('fs');
+const papa = require('papaparse');
 
 const contents = fs.readFileSync('./state-crime-data/2013.csv', 'utf8');
-console.log(contents);
+const parsedContents = papa.parse(contents);
+
+console.log(parsedContents.data);
+
+for (const item in parsedContents) {
+	console.log(item)
+}
+
+fs.writeFileSync('./test.json', JSON.stringify(parsedContents.data, null, 2))
