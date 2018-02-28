e70199004e6a833a9af87998b195571e74652850
arnav-aggarwal <arnav.aggarwal@thecontrolgroup.com>
2018-02-28 11:08:32 -0800

    Purge all unneeded data from generated JSON

--- index.js
+++ index.js
@@ -2,7 +2,23 @@ const fs = require('fs');
 const papa = require('papaparse');
 
 const contents = fs.readFileSync('./state-crime-data/2013.csv', 'utf8');
-const parsedContents = papa.parse(contents);
-parsedContents.data.splice(0, 3);
+let data = papa.parse(contents).data;
 
-fs.writeFileSync('./test.json', JSON.stringify(parsedContents.data, null, 2))
+// Remove unneeded rows
+data.splice(0, 3);
+data.splice(-12, 12);
+
+const columnTitles = data.splice(0, 1);
+data = data
+	.map(arr => {
+		// Remove legacy definition rape column, we will use new definition
+		arr.splice(5, 1);
+
+		// Change '\n' to '_'
+		arr = arr.map(str => str.replace(/\n/g, '_'));
+		return arr;
+	})
+	.filter(arr => arr[0] || arr.includes('State Total') || arr.includes('Rate per 100,000 inhabitants'));
+
+console.log(columnTitles);
+fs.writeFileSync('./test.json', JSON.stringify(data, null, 2));
