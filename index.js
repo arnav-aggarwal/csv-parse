bf623c12f893c349bd977cbac1df092503055c57
arnav-aggarwal <arnav.aggarwal@thecontrolgroup.com>
2018-02-28 09:59:42 -0800

    Remove unneeded first 3 rows

--- index.js
+++ index.js
@@ -3,11 +3,6 @@ const papa = require('papaparse');
 
 const contents = fs.readFileSync('./state-crime-data/2013.csv', 'utf8');
 const parsedContents = papa.parse(contents);
-
-console.log(parsedContents.data);
-
-for (const item in parsedContents) {
-	console.log(item)
-}
+parsedContents.data.splice(0, 3);
 
 fs.writeFileSync('./test.json', JSON.stringify(parsedContents.data, null, 2))
