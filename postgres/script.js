a2981995b6b0f73e7396aaefdbdfb9348b4a9bdd
arnav-aggarwal <arnav.aggarwal@thecontrolgroup.com>
2018-03-26 14:09:46 -0700

    shit works

--- postgres/script.js
+++ postgres/script.js
@@ -1,3 +1,7 @@
+function formatState(state) {
+	return state.split(' ').map(word => word[0].toUpperCase() + word.slice(1).toLowerCase()).join(' ');
+}
+
 function script(data) {
 	const columnTitles = data.splice(0, 1)[0];
 	const finalData = [];
@@ -12,7 +16,12 @@ function script(data) {
 		delete obj.state_original;
 		delete obj.city_original;
 		delete obj.id;
-		
+
+		if (obj.state === undefined) {
+			return;
+		}
+
+		obj.state = formatState(obj.state);
 		finalData.push(obj);
 	});
 
