95df11604b30db06a68d8eb7332becca6447ea9d
arnav-aggarwal <arnav.aggarwal@thecontrolgroup.com>
2018-03-26 13:44:05 -0700

    Write starter script

--- postgres/index.js
+++ postgres/index.js
@@ -2,6 +2,34 @@ const fs = require('fs');
 const papa = require('papaparse');
 const script = require('./script');
 
+function backfillLocation(data) {
+	for (let i = 0; i < data.length; i++) {
+		const currentItem = data[i];
+		const { county, county_source, state_fips, county_fips, lat, lng, zips } = currentItem;
+		const props = [county, county_source, state_fips, county_fips, lat, lng, zips];
+		const missingData = props.all(x => x === undefined);
+
+		if (!missingData) {
+			continue;
+		}
+
+		// find an item that has the same state & city and these properties
+		const missingDataObject = data.find(item => item.state === currentItem.state && item.city === currentItem.city && item.county !== undefined);
+
+		if (missingDataObject === undefined) {
+			continue;
+		}
+		
+		currentItem.county = missingDataObject.county;
+		currentItem.county_source = missingDataObject.county_source;
+		currentItem.state_fips = missingDataObject.state_fips;
+		currentItem.county_fips = missingDataObject.county_fips;
+		currentItem.lat = missingDataObject.lat;
+		currentItem.lng = missingDataObject.lng;
+		currentItem.zips = missingDataObject.zips;
+	}
+}
+
 const dataFolderPath = './data/';
 const fileNames = ['old.csv', 'new.csv'];
 const data = [];
@@ -13,6 +41,8 @@ fileNames.forEach(fileName => {
 	const json = script(parsedContents);
 	data.push(...json);
 });
+
+backfillLocation(data);
 
 fs.writeFileSync(`./formatted-data/test.json`, JSON.stringify(data, null, 2));
 
