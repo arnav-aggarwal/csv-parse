a92a3d62d5c856ba626b91062ee4a3962b0c0040
arnav-aggarwal <arnav.aggarwal@thecontrolgroup.com>
2018-04-02 16:04:04 -0700

    Fix script - state_fips is now backfilled

--- postgres/backfill.js
+++ postgres/backfill.js
@@ -1,4 +1,6 @@
 module.exports = function backfillLocation(data) {
+	console.log('1st loop started...');
+	console.time('1st loop');
 	for (let i = 0; i < data.length; i++) {
 		const currentItem = data[i];
 		const { county, county_source, state_fips, county_fips, lat, lng, zips } = currentItem;
@@ -24,4 +26,36 @@ module.exports = function backfillLocation(data) {
 		currentItem.lng = missingDataObject.lng;
 		currentItem.zips = missingDataObject.zips;
 	}
+	console.timeEnd('1st loop');
+
+	console.time('2nd loop');
+	for (let i = 0; i < data.length; i++) {
+		const currentItem = data[i];
+
+		if (currentItem.state_fips !== undefined) {
+			continue;
+		}
+
+		// find an item that has the same state & city and these properties
+		const missingDataObject = data.find(item => item.state === currentItem.state && item.state_fips !== undefined);
+
+		if (missingDataObject === undefined) {
+			continue;
+		}
+
+		currentItem.state_fips = missingDataObject.state_fips;
+	}
+	console.timeEnd('2nd loop');
+
+	console.time('3rd loop');
+	let noFips = 0;
+	for (let i = 0; i < data.length; i++) {
+		if (data[i].state_fips === undefined) {
+			console.log(data[i]);
+			noFips++;
+		}
+	}
+	console.timeEnd('3rd loop');
+
+	console.log('No fips count:', noFips);
 };
